6ad785593c2bd7bd255719e43ef6bcc9116ba6db
AlexTruba <straber@ukr.net>
2016-09-18 19:53:34 +0300

    Project Euler
    Find the maximum total from top to bottom of the triangle

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MaxPathSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Program().Result());
+            Console.ReadLine();
+        }
+        public int Result()
+        {
+            int[] res = new int []{0};
+            int[] list;
+            using (StreamReader sr  = new StreamReader("path.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    list = Array.ConvertAll(line.Split(' '), s => int.Parse(s));
+
+                    for (int i = 1; i < list.Length-1; i++) list[i] +=  Math.Max(res[i - 1],res[i]);
+
+                    list[0] += res[0];
+                    list[list.Length - 1] += res[res.Length-1];
+
+                    res = list;
+                }
+            }
+            return res.Max();
+        }
+    }
+}
